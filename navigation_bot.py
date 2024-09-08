7b4209ede5a88c5fa1aeba84d59f3a440d6c5919
antONget <64537429+antONget@users.noreply.github.com>
2024-09-08 20:17:33 +0300

    Update navigation_bot.py

--- navigation_bot.py
+++ navigation_bot.py
@@ -5,6 +5,9 @@ from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
 from handlers import admin_main_handlers, admin_add_card_handlers, user_handler, admin_delete_card_handler, \
     admin_edit_card_handler, other_handlers
+from aiogram.types import FSInputFile
+from aiogram.types import ErrorEvent
+import traceback
 # Инициализируем logger
 logger = logging.getLogger(__name__)
 
@@ -14,8 +17,8 @@ async def main():
     # Конфигурируем логирование
     logging.basicConfig(
         level=logging.INFO,
-        filename="py_log.log",
-        filemode='w',
+        # filename="py_log.log",
+        # filemode='w',
         format='%(filename)s:%(lineno)d #%(levelname)-8s '
                '[%(asctime)s] - %(name)s - %(message)s')
 
@@ -29,13 +32,24 @@ async def main():
     bot = Bot(token=config.tg_bot.token)
     dp = Dispatcher()
     # Регистрируем router в диспетчере
+    dp.include_router(admin_add_card_handlers.router)
     dp.include_router(user_handler.router)
     dp.include_router(admin_main_handlers.router)
-    dp.include_router(admin_add_card_handlers.router)
     dp.include_router(admin_delete_card_handler.router)
     dp.include_router(admin_edit_card_handler.router)
     dp.include_router(other_handlers.router)
 
+    @dp.error()
+    async def error_handler(event: ErrorEvent):
+        logger.critical("Критическая ошибка: %s", event.exception, exc_info=True)
+        await bot.send_message(chat_id=843554518,
+                               text=f'{event.exception}')
+        formatted_lines = traceback.format_exc()
+        text_file = open('error.txt', 'w')
+        text_file.write(str(formatted_lines))
+        text_file.close()
+        await bot.send_document(chat_id=843554518,
+                                document=FSInputFile('error.txt'))
 
     # Пропускаем накопившиеся update и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
